cbebad3edc12973c408ab695876926671d359140
William Kent <wjk@sunsol.me>
2017-02-05 19:36:50 -0500

    Finish dotnet-build-wrapper.sh
    This script now builds the NuGet packages as well.
    It also will exit immediately if a subcommand failed.

--- dotnet-build-wrapper.sh
+++ dotnet-build-wrapper.sh
@@ -11,7 +11,15 @@ fi
 
 MY_DIR=$(cd $(dirname $0) && pwd)
 
+set -e
+
 mkdir -p $MY_DIR/bin/native
 pushd $MY_DIR/bin/native > /dev/null
 $MY_DIR/src/corehost/build.sh --rid $RID --arch $ARCH --hostver 1.0.0 --apphostver 1.0.0 --fxrver 1.0.0 --policyver 1.0.0 --commithash $(git rev-parse HEAD)
+popd > /dev/null
+
+mkdir -p $MY_DIR/bin/pkg
+pushd $MY_DIR/bin/pkg > /dev/null
+$MY_DIR/pkg/init-tools.sh
+$MY_DIR/pkg/build.sh
 popd > /dev/null
