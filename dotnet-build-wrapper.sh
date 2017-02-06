362766362a44d1b4c1947cf10c2df6ca1748938a
William Kent <wjk@sunsol.me>
2017-02-05 19:32:36 -0500

    Add dotnet-build-wrapper.sh
    This script is an alternative to the build.sh provided by
    upstream. The upstream build.sh does a lot of downloading
    and processing of files that is overkill for my needs. This
    script only builds the corehost and the NuGet packages, and
    does not invoke the bespoke core-setup build system.

---
+++ dotnet-build-wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# These variables should be set by the script calling dotnet-build-wrapper.sh.
+RID=$1
+ARCH=$2
+
+if [ "$RID" == "" -o "$ARCH" == "" ]; then
+    echo "usage: $0 <target_rid> <target_arch>" 2>&1
+    exit 1
+fi
+
+MY_DIR=$(cd $(dirname $0) && pwd)
+
+mkdir -p $MY_DIR/bin/native
+pushd $MY_DIR/bin/native > /dev/null
+$MY_DIR/src/corehost/build.sh --rid $RID --arch $ARCH --hostver 1.0.0 --apphostver 1.0.0 --fxrver 1.0.0 --policyver 1.0.0 --commithash $(git rev-parse HEAD)
+popd > /dev/null
